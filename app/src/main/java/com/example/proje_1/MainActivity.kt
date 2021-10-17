aacafc0a6aead065b12f54c310377329d23ffd9d
RasoulAfshari <80676517+RasoulAfshari@users.noreply.github.com>
2021-10-17 11:32:18 +0330

    start project

--- app/src/main/java/com/example/proje_1/MainActivity.kt
+++ app/src/main/java/com/example/proje_1/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.proje_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val frm1 : FrameLayout=findViewById(R.id.ac_main_frm1)
+        val frm2 : FrameLayout=findViewById(R.id.ac_main_frm2)
+        val edit1 : EditText=findViewById(R.id.ac_main_edit_text)
+        val btn1 : Button = findViewById(R.id.ac_main_btn)
+        val btn2 : Button = findViewById(R.id.ac_main_btn2)
+
+        btn1.setOnClickListener {
+            if(edit1.length()==0) {
+                frm1.visibility = View.GONE
+                frm2.visibility = View.VISIBLE
+            }
+        }
+        btn2.setOnClickListener {
+
+            frm1.visibility=View.VISIBLE
+            frm2.visibility=View.GONE
+        }
     }
 }
